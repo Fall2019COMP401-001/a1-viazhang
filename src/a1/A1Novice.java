22fea9ca7f3cac58dce1a04f52df001d7c5ef1eb
via <via@192.168.0.4>
2019-09-02 02:40:27 -0400

    First Try

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -2,6 +2,8 @@ package a1;
 
 import java.util.Scanner;
 
+import javax.print.DocFlavor.STRING;
+
 public class A1Novice {
 
 	public static void main(String[] args) {
@@ -10,5 +12,25 @@ public class A1Novice {
 
 		// Your code follows here.
 		
+		int totalNumber = scan.nextInt();
+		
+		for (int i = 0; i < totalNumber; i++) {
+			  System.out.println(i);
+			  String fName = scan.nextLine();
+			  String lName = scan.nextLine();
+			  int itemNo = scan.nextInt();
+			  double totalmoney = 0;
+			  for (int j = 0; j < itemNo; i++) {
+				  System.out.println(i);
+				  int numberBought = scan.nextInt();
+				  String itemName = scan.nextLine();
+				  double price = scan.nextDouble();
+				  totalmoney = totalmoney + numberBought * price;
+			  System.out.println(fName);
+			  System.out.println(lName);
+			  System.out.println(totalmoney);
+						  
+				}
+			}
 	}
 }
