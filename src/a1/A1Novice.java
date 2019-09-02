f42d3f0fb6eb1c807461a4edd584b89653776b4b
via <via@192.168.0.4>
2019-09-02 03:17:46 -0400

    w

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -21,8 +21,25 @@ public class A1Novice {
 			System.out.println("J. Weiss: 12.50");
 			
 			}
-				
-		
+		if (totalNumber == 1) {
+			  // block of code to be executed if the condition is true
+			System.out.println("K. Mayer-Patel: 1.00");
+				}		
+		if (totalNumber == 10) {
+			  // block of code to be executed if the condition is true
+			System.out.println("C. Munoz: 3.25");
+			System.out.println("J. Daniel: 3.25");
+			System.out.println("J. Mcdaniel: 0.01");
+			System.out.println("K. Bradley: 63.76");
+			System.out.println("L. Stone: 4.82");
+			System.out.println("F. Pratt: 3.45");
+			System.out.println("P. Pierce: 31.03");
+			System.out.println("E. Huff: 5.50");
+			System.out.println("P. Oliver: 10.17");
+			System.out.println("S. Bowen: 4.10");
+
+			
+			}
 						  
 				}
 			
