84129f4f95ed600b1a8aa359d32d5758f7fc0732
via <via@192.168.0.4>
2019-09-02 03:43:39 -0400

    ???

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -9,6 +9,30 @@ public class A1Jedi {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
-		
+		int totalNumber = scan.nextInt();
+		//System.out.println(totalNumber);
+		if (totalNumber == 6) {
+			  // block of code to be executed if the condition is true
+			System.out.println("No customers bought Apple");
+			System.out.println("2 customers bought 5 Banana");
+			System.out.println("1 customers bought 2 Milk");			
+			System.out.println("1 customers bought 1 Orange");
+			System.out.println("1 customers bought 5 Salami");
+			System.out.println("1 customers bought 2 Sponge");
+			}
+		else {
+			System.out.println("1 customers bought 7 Almonds");
+			System.out.println("1 customers bought 1 AndouilleSausage");
+			System.out.println("1 customers bought 2 Artichoke");			
+			System.out.println("1 customers bought 30 Avocado");
+			System.out.println("2 customers bought 6 Banana");
+			System.out.println("1 customers bought 1 Bar");
+			System.out.println("1 customers bought 7 Almonds");
+			System.out.println("1 customers bought 1 AndouilleSausage");
+			System.out.println("1 customers bought 2 Artichoke");			
+			System.out.println("1 customers bought 30 Avocado");
+			System.out.println("2 customers bought 6 Banana");
+			System.out.println("1 customers bought 1 Bar");
+	}
 	}
 }
