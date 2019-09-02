fe8bcbab1d11fee5b5365d403aa9b780f742779a
via <via@192.168.0.4>
2019-09-02 02:48:00 -0400

    TT

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -17,15 +17,23 @@ public class A1Novice {
 		for (int i = 0; i < totalNumber; i++) {
 			  System.out.println(i);
 			  String fName = scan.nextLine();
+			  System.out.println(fName);
 			  String lName = scan.nextLine();
+			  System.out.println(lName);
 			  int itemNo = scan.nextInt();
+			  System.out.println(itemNo);
 			  double totalmoney = 0.0;
+			  System.out.println(totalmoney);
 			  for (int j = 0; j < itemNo; i++) {
 				  System.out.println(i);
 				  int numberBought = scan.nextInt();
+				  System.out.println(numberBought);
 				  String itemName = scan.nextLine();
+				  System.out.println(itemName);
 				  double price = scan.nextDouble();
+				  System.out.println(price);
 				  totalmoney = totalmoney + numberBought * price;
+				  System.out.println(totalmoney);
 			  System.out.println(fName);
 			  System.out.println(lName);
 			  System.out.println(totalmoney);
