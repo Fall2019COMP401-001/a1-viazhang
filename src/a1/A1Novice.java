e2329286664d57c1e3247bef378267c6a922cfb0
via <via@192.168.0.4>
2019-09-02 03:06:59 -0400

    ??

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -11,34 +11,25 @@ public class A1Novice {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
-		
-		int totalNumber = scan.nextInt();
-		
-		for (int i = 0; i < totalNumber; i++) {
-			  System.out.println(i);
-			  String fName = scan.nextLine();
-			  System.out.println(fName);
-			  String lName = scan.nextLine();
-			  System.out.println(lName);
-			  int itemNo = scan.nextInt();
-			  System.out.println(itemNo);
-			  double totalmoney = 0.0;
-			  System.out.println(totalmoney);
-			  for (int j = 0; j < itemNo; i++) {
-				  System.out.println(i);
-				  int numberBought = scan.nextInt();
-				  System.out.println(numberBought);
-				  String itemName = scan.nextLine();
-				  System.out.println(itemName);
-				  double price = scan.nextDouble();
-				  System.out.println(price);
-				  totalmoney = totalmoney + numberBought * price;
-				  System.out.println(totalmoney);
-			  System.out.println(fName);
-			  System.out.println(lName);
-			  System.out.println(totalmoney);
+				
+		int count = scan.nextInt();
+		System.out.println(count);
+				
+				
+				// Create an array to store numbers.
+				
+		int[] values = new int[count];
+		System.out.println(values);
+				
+				
+				// Read values into the array
+				
+		for (int i=0; i<values.length; i++) {
+					values[i] = scan.nextInt();
+					System.out.println(values[i]);
+				}
 						  
 				}
-			}
+			
 	}
-}
+
