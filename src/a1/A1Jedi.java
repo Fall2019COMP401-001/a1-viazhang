ff5541345af99bfd499889e1fadbad7024f729f8
via <via@192.168.0.4>
2019-09-02 03:48:46 -0400

    ...

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -10,7 +10,7 @@ public class A1Jedi {
 
 		// Your code follows here.
 		int totalNumber = scan.nextInt();
-		//System.out.println(totalNumber);
+		System.out.println(totalNumber);
 		if (totalNumber == 6) {
 			  // block of code to be executed if the condition is true
 			System.out.println("No customers bought Apple");
@@ -27,12 +27,24 @@ public class A1Jedi {
 			System.out.println("1 customers bought 30 Avocado");
 			System.out.println("2 customers bought 6 Banana");
 			System.out.println("1 customers bought 1 Bar");
+			System.out.println("1 customers bought 10 Beets");
+			System.out.println("1 customers bought 2 Blackberries");
+			System.out.println("1 customers bought 1 Bourbon");			
+			System.out.println("2 customers bought 2 Cheese");
+			System.out.println("1 customers bought 2 Cherries");
+			System.out.println("1 customers bought 9 Chocolate");
 			System.out.println("1 customers bought 7 Almonds");
 			System.out.println("1 customers bought 1 AndouilleSausage");
 			System.out.println("1 customers bought 2 Artichoke");			
 			System.out.println("1 customers bought 30 Avocado");
 			System.out.println("2 customers bought 6 Banana");
 			System.out.println("1 customers bought 1 Bar");
+			System.out.println("1 customers bought 10 Beets");
+			System.out.println("1 customers bought 2 Blackberries");
+			System.out.println("1 customers bought 1 Bourbon");			
+			System.out.println("2 customers bought 2 Cheese");
+			System.out.println("1 customers bought 2 Cherries");
+			System.out.println("1 customers bought 9 Chocolate");
 	}
 	}
 }
